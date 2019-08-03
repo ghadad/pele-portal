85b867ab7aa7cdb93e9d0899943d3a3d6701ae32
Golan Hadad <ghadad@gmail.com>
2019-08-04 01:09:21 +0300

    dynamic component rendering using ui config

--- src/components/mainmenu.js
+++ src/components/mainmenu.js
@@ -1,14 +1,15 @@
 Mainmenu = Vue.component("mainmenu", {
     template: `<div><h1>hello</h1>
-    <component v-for="cmp of ui.hr_807.componenets" :is="cmp"  :data="data">
+    <component v-if="$route.params.comp" v-for="cmp of ui[$route.params.comp].componenets" :is="cmp"  :data="data">
     <h2>{{ui.hr_807.title}}</h2><slot></slot></component>
-    <component v-for="cmp of ui.hr_242.componenets" :is="cmp"  :data="data">
-    <h2>{{ui.hr_242.title}}</h2><slot></slot></component></div>`,
+   
+    {{$route.params}}</div>`,
     data: function () {
         return {
             data: {
                 dd: 2,
-                title: ui.hr_807.title
+                title: ui.hr_807.title,
+                router: this.$route.params
             },
             ui: ui,
 
@@ -16,7 +17,12 @@ Mainmenu = Vue.component("mainmenu", {
     },
     mounted: function () {
         peleServices.request("ddd", {
-            d: 2
+            d: 2,
         });
+    },
+    computed: {
+        router: function () {
+            return this.$route;
+        }
     }
 });
--- src/router.js
+++ src/router.js
@@ -1,25 +1,25 @@
-let  routes = [{
-    path: '/',
-    component: Mainmenu
-},
-{
-    path: '/airports',
-    component: Mainmenu
-},
-{
-    path: '/login',
-    component: Mainmenu
-},
-{
-    path: '/cams',
-    component: Mainmenu
-}
+let routes = [{
+        path: '/',
+        component: Mainmenu
+    },
+    {
+        path: '/maof/:comp',
+        component: Mainmenu
+    },
+    {
+        path: '/login',
+        component: Mainmenu
+    },
+    {
+        path: '/cams',
+        component: Mainmenu
+    }
 ];
 
 Vue.use(VueRouter);
 let router = new VueRouter({
     //mode: 'history',
-    routes:routes // short for `routes: routes`
+    routes: routes // short for `routes: routes`
 })
 
 router.beforeEach((to, from, next) => {
